16e5f2c0e9c75dafe74ba32ad05a29e8f2e5201c
tunnckoCore <mameto_100@mail.bg>
2015-01-30 04:39:19 +0200

    add optional 2nd arg for short stack trace

--- index.js
+++ index.js
@@ -10,29 +10,52 @@
 var path = require('path');
 var fmt = require('util').format;
 
-module.exports = function handleErrors(label) {
+module.exports = function handleErrors(label, stack) {
+  checkType(label, 'label');
+
   return {
     error: newError,
     type: newTypeError
   };
 
   function newError(err, cb) {
-    err = new Error(fmt('%s: %s', label, err));
+    checkType(err, 'msg');
+    err = new Error(fmt('[%s] %s', label, err));
+    err = stack ? buildShortStack(err) : err;
 
     if (!cb) {
       throw err;
     }
-
     return cb(err);
   }
 
   function newTypeError(err, cb) {
-    err = new TypeError(fmt('%s: %s', label, err));
+    checkType(err, 'msg');
+    err = new TypeError(fmt('[%s] %s', label, err));
+    err = stack ? buildShortStack(err) : err;
 
     if (!cb) {
       throw err;
     }
-
     return cb(err);
   }
 };
+
+function buildShortStack(err) {
+  var shortStack = err.stack.split('\n').filter(Boolean).filter(function(line) {
+    return /\//.test(line)
+  });
+
+  shortStack[0] = err.toString();
+  err.shortStack = shortStack.join('\n');
+  return err;
+}
+
+function checkType(arg, name) {
+  if (!arg) {
+    throw new Error('[handle-errors] should have `' + name + '`')
+  }
+  if (typeof arg !== 'string') {
+    throw new TypeError('[handle-errors] expect `' + name + '` be string')
+  }
+}
